46627c7948fe2181b668be83fda576239a3f5628
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2024-07-19 20:32:20 +0000

    added mongodb_connection for entity adn paused at 48:13 min

--- demo.py
+++ demo.py
@@ -1,11 +1,48 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+import os
+from pymongo import MongoClient
 
-# logging.info("Welcome to our custom log")
+# Retrieve MongoDB credentials and host details from environment variables
+mongo_username = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+mongo_password = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
+mongo_db_url = os.getenv('MONGODB_URL')
 
+print(mongo_db_url)
 
+# Database and Collection names
+database_name = "US_VISA"
+collection_name = "visa_data"
+
+# Connect to the MongoDB database
+client = MongoClient(mongo_db_url)
+db = client[database_name]
+collection = db[collection_name]
+
+# Fetch all documents from the collection and print them
 try:
-    a = 2/0
+    documents = collection.find()
+    for document in documents:
+        print(document)
 except Exception as e:
-    raise USvisaException(e, sys)
+    print(f"An error occurred: {e}")
+
+
+mongo_db_url = os.getenv('MONGODB_URL')
+
+print(mongo_db_url)
+
+# Database and Collection names
+database_name = "US_VISA"
+collection_name = "visa_data"
+
+# Connect to the MongoDB database
+client = MongoClient(mongo_db_url)
+db = client[database_name]
+collection = db[collection_name]
+
+# Fetch all documents from the collection and print them
+try:
+    documents = collection.find()
+    for document in documents:
+        print(document)
+except Exception as e:
+    print(f"An error occurred: {e}")
